c20d7fc97a3f859348883dfde278055660400c56
ehmedjamil <67521944+ehmedjamil@users.noreply.github.com>
2022-07-23 18:14:51 +0300

    Add files via upload

--- ScriptExample.js
+++ ScriptExample.js
@@ -1,23 +1,26 @@
 var obj = JSON.parse($response.body);
 
 obj =  {
-  "status": "ok",
-  "is_valid": true,
-  "expiration_date": "2023-07-25T12:41:23Z",
-  "expiration_date_unix": 1690046947,
-  "is_trial": false,
-  "processing_count": 0,
-  "is_introductory_used": true,
-  "product_id": "",
-  "promotional_offer_id": "",
-  "is_grace_period": false,
-  "auto_renew_enabled": true,
-  "is_in_billing_retry_period": false,
-  "subscription_type": "annual",
-  "platform": "ios",
-  "device_user_info": {
-    "auth_type": "",
-    "subscription_valid": true
-  }
-}
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 $done({body: JSON.stringify(obj)});
+
